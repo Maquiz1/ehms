afcb8fac8418d20fa0815daf253212df51a92d0b
Maquiz1 <manquiz92@gmail.com>
2025-03-24 23:34:11 +0300

    Fix Login And Log OUt

--- clinic/forms.py
+++ clinic/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
@@ -12,3 +12,19 @@ class CustomUserCreationForm(UserCreationForm):
             'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter password'}),
             'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm password'}),
         }
+
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your username',
+        }),
+        label="Username",
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your password',
+        }),
+        label="Password",
+    )
--- clinic/views.py
+++ clinic/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.urls import reverse
-from .forms import CustomUserCreationForm
+from django.contrib.auth.views import LoginView
+from .forms import CustomUserCreationForm, CustomLoginForm
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
 
 def hospital_dashboard(request):
     return render(request, "clinic/hospital_dashboard.html", {})
 
-def sign_up(request):
-    if request.method == "POST":
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect(reverse("hospital_dashboard"))
-    else:
-        form = CustomUserCreationForm()
-    return render(request, "registration/sign_up.html", {"form": form})
+class SignUpView(FormView):
+    template_name = "registration/sign_up.html"
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("hospital_dashboard")
 
+    def form_valid(self, form):
+        user = form.save()  # Save the user
+        login(self.request, user)  # Log the user in
+        return super().form_valid(form)
 
 def hospital_admin(request):
     return render(request, "clinic/hospital_admin.html", {})
@@ -40,3 +41,8 @@ def medical_dashboard(request):
 #     else:
 #         form = PatientForm()
 #     return render(request, "clinic/add_patients.html", {"form": form})
+
+
+class CustomLoginView(LoginView):
+    template_name = "registration/login.html"
+    form_class = CustomLoginForm
